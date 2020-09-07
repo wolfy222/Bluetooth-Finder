4844e944e74cb7bed127156c75f3876253e6d826
Aadarsh Agarwal <aadarshagarwal2229@gmail.com>
2020-09-07 16:25:21 +0530

    Updating App

--- app/src/main/java/com/example/bluetoothfinder/MainActivity.java
+++ app/src/main/java/com/example/bluetoothfinder/MainActivity.java
@@ -3,7 +3,13 @@ package com.example.bluetoothfinder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
@@ -17,13 +23,32 @@ public class MainActivity extends AppCompatActivity
     // POWER EVERYTHING IN BLUETOOTH
     BluetoothAdapter bluetoothAdapter;
 
+    // CREATING RECEIVER
+    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent)
+        {
+            String action = intent.getAction();
+            Log.i("Action",action);
+            //  AFTER FINISHING SEARCH
+            if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action))
+            {
+                statusTextView.setText("Finished");
+                searchButton.setEnabled(true);
+            }
+
+        }
+    };
+
 
     public void searchClicked(View view)
     {
         statusTextView.setText("Searching...");
         searchButton.setEnabled(false);
+        bluetoothAdapter.startDiscovery();
 
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -34,5 +59,20 @@ public class MainActivity extends AppCompatActivity
         listView = findViewById(R.id.listView);
         statusTextView = findViewById(R.id.statusTextView);
         searchButton = findViewById(R.id.searchButton);
+
+        // SETTING UP THE BLUETOOTH
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        // IT HELP US FIND WHAT WE ARE LOOKING FOR
+        IntentFilter intentFilter = new IntentFilter();
+        // FILTERS MESSAGE
+        intentFilter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
+        intentFilter.addAction(BluetoothDevice.ACTION_FOUND);
+        intentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        intentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        // REGISTER INTENT FILTER
+        registerReceiver(broadcastReceiver, intentFilter);
+
+
     }
 }
